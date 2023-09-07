2462ac48a458b8de0fb6ea1780bdfdcb67bebf04
Name <barmatine99@gmail.com>
2023-09-07 07:01:22 +0300

    Main. Remade

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,26 +1,39 @@
 public class Radio {
-    public int volume;
-    public int can;
+    private int volume;
+    private int can;
 
-    public Radio() {
-        volume = 0;
-        can = 0;
+    public void setVolume(int vol) {
+        volume = vol;
     }
 
-    public Radio(int vol, int c) {
-        volume = vol;
-        can = c;
+    public void setCan(int cann) {
+        can = cann;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getCan() {
+        return can;
     }
 
     public void up() {
-        if(volume < 100) volume++;
+        if (volume < 100) {
+            volume++;
+        }
     }
+
     public void down() {
-        if(volume > 0) volume--;
+        if (volume > 0) {
+            volume--;
+        }
     }
+
     public void next() {
         can = (can + 1) % 10;
     }
+
     public void prev() {
         can = (can - 1 + 10) % 10;
     }
