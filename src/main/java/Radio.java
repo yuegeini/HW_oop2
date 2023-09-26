b4666126d23b87f43ac970921fb979085b1571c6
Name <barmatine99@gmail.com>
2023-09-26 10:39:26 +0300

    Reformat

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,33 +1,36 @@
 public class Radio {
     private int volume;
     private int can;
-
     private int maxCan;
-
-    public void setVolume(int vol) {
-        volume = vol;
-    }
-    public void setCan(int cann) {
-        can = cann;
-    }
-    public int getVolume() {
-        return volume;
-    }
-    public int getCan() {
-        return can;
-    }
-
 
     public Radio() {
         volume = 0;
         can = 0;
         maxCan = 10;
     }
+
     public Radio(int max_can) {
         maxCan = max_can;
         volume = 0;
         can = 0;
     }
+
+    public void setVolume(int vol) {
+        volume = vol;
+    }
+
+    public void setCan(int cann) {
+        can = cann;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getCan() {
+        return can;
+    }
+
 
 //    public Radio(int vol, int c) {
 //        volume = vol;
@@ -36,14 +39,17 @@ public class Radio {
 //    }
 
     public void up() {
-        if(volume < 100) volume++;
+        if (volume < 100) volume++;
     }
+
     public void down() {
-        if(volume > 0) volume--;
+        if (volume > 0) volume--;
     }
+
     public void next() {
         can = (can + 1) % maxCan;
     }
+
     public void prev() {
         can = (can - 1 + maxCan) % maxCan;
     }
